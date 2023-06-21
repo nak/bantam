26b8b450bf1ed31f29dcae81d50ba1c10f8aa003
nak <jrusnak69@gmail.com>
2023-06-20 21:48:56 -0700

    minor tweak to add repr to InvocationException

--- src/bantam/client.py
+++ src/bantam/client.py
@@ -116,6 +116,12 @@ class InvocationError(Exception):
     def __init__(self, message: str):
         self.message = message
 
+    def __str__(self):
+        return self.message
+
+    def __repr__(self):
+        return self.message
+
 
 # noinspection PyProtectedMember
 class WebInterface(ABC):
